d0aceac6515278b5b06daa1bedd64253828df536
memety <meliroxi@gmail.com>
2015-03-15 22:29:30 +0100

    Update dinner_2.js

--- js/game/dinner_2.js
+++ js/game/dinner_2.js
@@ -11,7 +11,7 @@ function Start_Dinner_2(){
 	switch($.waiting_action){
 		case "eat":
 			m("Ó, nélkülem kezdtél enni... Nagyon türelmetlen vagy.");
-			n("...ja.");
+			n("...öhhm, ja. Bocsi.");
 			break;
 		case "wait":
 			m("Elkezdhetted volna nélkülem is. Nem kellett volna megvárnod, hogy kihűljön.");
@@ -55,22 +55,22 @@ function Start_Dinner_2(){
 
 function Start_Dinner_2_1(){
 
-	n("Ó. Hmm... tanulok.")
+	n("Öhm, izé... tanulok?")
 	n("Ja, holnap tanulni fogok.");
 	m("Milyen tárgyat?");
 	n("Öhmm...");
 
 	Choose({
 		"Kémiát.": function(message){
-			$.studying_subject = "Chemistry";
+			$.studying_subject = "kémia";
 			Start_Dinner_2_2(message);
 		},
 		"Matekot.": function(message){
-			$.studying_subject = "Calculus";
+			$.studying_subject = "matematika";
 			Start_Dinner_2_2(message);
 		},
 		"Számtech-et.": function(message){
-			$.studying_subject = "Computer Science";
+			$.studying_subject = "számítástechnika";
 			Start_Dinner_2_2(message);
 		}
 	});
@@ -101,7 +101,7 @@ function Start_Dinner_2_2(message){
 			
 			$.lying_about_hanging_out = true;
 			m("Ó, mint egy legjobb barát?");
-			n("Öhm, nos...--");
+			n("Öhm, nos...");
 			m("Szóval ti csak szórakoztok tanulás helyett.");
 			n("Mi igenis tanulunk!");
 			m(". . .");
@@ -109,7 +109,7 @@ function Start_Dinner_2_2(message){
 			n("Nem hazudok.");
 			Buddy_1_point_5();
 		},
-		"Hát igen, mivel jó haverom.": function(message){
+		"Hát igen, mivel jó barátom.": function(message){
 			$.relationship = "friend";
 			Buddy_1(message);
 		}
@@ -207,7 +207,7 @@ function Buddy_4(message){
 		"Azt hiszed, meleg.": function(message){
 			n(message);
 			m("Azt!");
-			m("Ahogy te is gyanítod");
+			m("Ahogy te is gyanítod.");
 			Buddy_Choice();
 		},
 		"Ne mondd ezt a barátomról!!": function(message){
@@ -223,7 +223,7 @@ function Buddy_4(message){
 
 					m("Viszont azt te is elfogadod, hogy rossz dolog, ha valaki ilyen természetellenesnek tűnik.");
 					n("Éé-én ezt soha nem mondtam...");
-					m("Énn csak próbállak megóvni téged ettől! Mert ő tudod, úgy viselkedik, mint, tudod...");
+					m("Én csak próbállak megóvni téged ettől! Mert ő tudod, úgy viselkedik, mint, tudod...");
 					m("Egy meleg!");
 					Buddy_Choice();
 
@@ -232,48 +232,48 @@ function Buddy_4(message){
 
 				n(message);
 				m("Én csak próbálok őszinte lenni.");
-				m("But yes, even you agree that it's bad to be seen as 'not natural'.");
-				n("I never said--");
-				m("And I'm just looking out for you! Because he acts like, you know...");
-				m("A gay!");
+				m("Viszont azt te is elfogadod, hogy rossz dolog, ha valaki ilyen természetellenesnek tűnik.");
+				n("Éé-én ezt soha nem mondtam...");
+				m("Én csak próbállak megóvni téged ettől! Mert ő tudod, úgy viselkedik, mint, tudod...");
+				m("Egy meleg!");
 				Buddy_Choice();
 
 			}
 
 		},
-		"What do you mean, he's not natural?": Buddy_3
+		"Hogy érted azt, hogy természetellenes?": Buddy_3
 	});
 }
 
 function Buddy_Choice(){
 	if($.relationship=="friend"){
-		m("And since you say he's a 'good pal'...");
-		m("People might think you're a gay like him, too.");
+		m("És amikor azt mondod, hogy Jack jó barátod...");
+		m("Az emberek esetleg azt feltételezik, hogy te is meleg vagy, akárcsak ő.");
 	}
 	if($.relationship=="best friend"){
-		m("And since you say he's your BEST friend...");
-		m("People might think you're a gay like him, too.");
+		m("És amióta azt mondod, hogy ő a legjobb barátod...");
+		m("Az emberek esetleg azt feltételezik, hogy te is meleg vagy, akárcsak ő.");
 	}
 	Choose({
-		"Ha, he sure acts gay. Luckily, he's not.": function(message){
+		"Biztos vagyok benne, hogy csak megjátsza, mert szerencsére nem meleg.": function(message){
 			n(message);
-			m("See? You also think there's something not right about it.");
-			n("...sure.");
+			m("Látod? Te is észrevetted, hogy valami nincs rendben vele.");
+			n("...öhm, ja.");
 			Buddy_Aftermath();
 		},
-		"What's wrong with being gay?!": function(message){
+		"Mi a baj azzal, ha valaki meleg?!": function(message){
 			n(message);
-			m("Nothing! Nothing.");
+			m("Semmi! Semmi.");
 			Buddy_Aftermath();
 		},
-		"Maybe... my friend might be gay.": function(message){
+		"Lehet, hogy tényleg meleg.": function(message){
 
 			if($.relationship=="study" && !$.lying_about_relationship){
 				Buddy_Caught_Lying_1(message,function(message){
 					n(message);
-					m("Okay.");
-					m("Just don't lie to me.");
-					n("I won't.");
+					m("Oké.");
+					m("Csak ne hazudj nekem.");
+					n("Nem teszem.");
 					m(". . .");
 					Buddy_Aftermath();
 				});
@@ -289,18 +289,18 @@ function Buddy_Choice(){
 
 function Buddy_Aftermath(){
 
-	m("Don't get me wrong.");
-	m("I'm not saying those kind of people are bad!");
-	m("I just think... you should be careful around one of them.");
-	m("Jack might, you know, try to recruit you.");
+	m("Ne ítélj el.");
+	m("Én nem mondtam, hogy ezek az emberek rosszak!");
+	m("Én csak azt gondolom... hogy talán óvatosabbnak kellene lenned velük.");
+	m("Jack talán, tudod, próbál 'megtéríteni' téged.");
 
 	Show("clock_time","clock_1910");
 	Show("nicky","dinner_nicky_defiant");
 
 	Choose({
-		"what.": Buddy_Aftermath_2,
-		"whaaat.": Buddy_Aftermath_2,
-		"whaaaaaaaaaaaaaaat.": Buddy_Aftermath_2
+		"Mi?": Buddy_Aftermath_2,
+		"Mi van?!": Buddy_Aftermath_2,
+		"Hogy miiiiii?": Buddy_Aftermath_2
 	});
 }
 
@@ -308,28 +308,28 @@ function Buddy_Aftermath_2(message){
 	
 	n(message);
 
-	n("How do you even...");
-	n("Ugh, nevermind.");
-	m("Nick, I'm sorry you find me annoying.");
-	n("No, mom, stop doing th--");
-	m("Let's go back to talking about your grades.");
-	m("Now, what did you say you were studying tomorrow?");
+	n("Hogy vagy képes...");
+	n("Eh, mindegy.");
+	m("Nick, sajnálom, ha felbosszantottalak.");
+	n("Nem, anya, fejezd be ezt az...");
+	m("Térjünk vissza a jegyeidhez.");
+	m("Szóval, mit is mondtál, mit fogsz tanulni holnap?");
 
 	Show("nicky","dinner_nicky_sit");
 	n(". . .");
-	n("Errrmmmmm...");
+	n("Öhhhhhmmm...");
 
 	Choose({
-		"Compsci?": function(message){
-			$.studying_subject_2 = "Computer Science";
+		"Számtech-et?": function(message){
+			$.studying_subject_2 = "számítástechnikát";
 			Grades_Start(message);
 		},
-		"Chemistry?": function(message){
-			$.studying_subject_2 = "Chemistry";
+		"Kémiát?": function(message){
+			$.studying_subject_2 = "kémiát";
 			Grades_Start(message);
 		},
-		"Calculus?": function(message){
-			$.studying_subject_2 = "Calculus";
+		"Matekot?": function(message){
+			$.studying_subject_2 = "matematikát";
 			Grades_Start(message);
 		}
 	});
@@ -352,29 +352,29 @@ function Grades_Start(message){
 }
 
 function Grades_Start_1(){
-	m("You first told me it was "+$.studying_subject+".");
-	m("Now you tell me it's "+$.studying_subject_2+"?");
+	m("Először "+$.studying_subject+" mondtál.");
+	m("Most pedig "+$.studying_subject_2+"?");
 	$.lying_about_studying = true;
-	n("Mom, I was just confus--");
+	n("Anya, csak összezavarodtam...");
 	if($.lying_about_hanging_out || $.lying_about_relationship){
-		m("This is TWICE you've lied to me during this dinner.");
-		n("I didn't lie about--");
+		m("Ez a MÁSODIK alkalom, hogy ma este hazudsz nekem!");
+		n("De én nem akartam erről hazudni...");
 	}
-	m("Either way, your grades in both subjects are terrible.");
+	m("Egyébként a jegyeid mindkét tantárgyból borzalmasak.");
 	n(". . .");
 	Grades_Explaining();
 }
 
 function Grades_Start_2(){
-	m("You hesitated for a moment there.");
-	n("I was eating.");
-	m("Okay.");
+	m("Hezitáltál egy pillanatig.");
+	n("Csak ettem.");
+	m("Oké.");
 	if($.lying_about_hanging_out){
-		m("I wonder if you're studying with Jack at all, or just always hanging out.");
-		n("We study.");
+		m("Én csak kíváncsi vagyok, hogy Jack-kel tényleg végig tanultok, vagy hülyeségekkel foglalkoztok.");
+		n("Végig tanulunk.");
 	}
 	m(". . .");
-	m("Still, your grades in your "+$.studying_subject_2+" class are terrible.");
+	m("Ennek ellenére a jegyeid ugyanolyan borzalmasak, már ami a "+$.studying_subject_2+" illeti.");
 	n(". . .");
 	Grades_Explaining();
 }
